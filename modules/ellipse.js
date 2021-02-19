22c0c4009a378608df874b6988e52650c6f40b74
Martin Krauskopf <martin.krauskopf@gmail.com>
2021-02-19 14:04:01 +0100

    Ellipse Scene: show orbital eccentricity

--- modules/ellipse.js
+++ modules/ellipse.js
@@ -7,6 +7,7 @@ const colors = {
   semiMajor: "#ff531e",
   semiMinor: "#0f93d4",
   linearEccentricity: "#1db11d",
+  orbitalEccentricity: "magenta",
 };
 
 class Ellipse {
@@ -15,21 +16,25 @@ class Ellipse {
     this.a = undefined; // semi-major axis
     this.b = undefined; // semi-minor axis
     this.le = undefined; // linear eccentricity (center to focus)
+    this.oe = undefined; // orbital eccentricity
   }
 
   updateStatus() {
     this.statusEl.innerHTML =
-      span(`semi-major axis: ${this.a}`, colors.semiMajor) +
+      span(`semi-major axis: ${this.a.toFixed(2)}`, colors.semiMajor) +
       br() +
-      span(`semi-minor axis: ${this.b}`, colors.semiMinor) +
+      span(`semi-minor axis: ${this.b.toFixed(2)}`, colors.semiMinor) +
       br() +
-      span(`linear eccentricity: ${this.le}`, colors.linearEccentricity);
+      span(`linear eccentricity: ${this.le.toFixed(2)}`, colors.linearEccentricity) +
+      br() +
+      span(`orbital eccentricity: ${this.oe.toFixed(2)}`, colors.orbitalEccentricity);
   }
 
   render({ ctx, width, height }) {
-    this.b = Math.round(Math.min((width * 0.8) / 3, height / 2 - 100));
-    this.a = Math.round(this.b * 1.5);
-    this.le = Math.round(Math.sqrt(this.a ** 2 - this.b ** 2));
+    this.b = Math.min((width * 0.8) / 3, height / 2 - 100);
+    this.a = this.b * 1.5;
+    this.le = Math.sqrt(this.a ** 2 - this.b ** 2);
+    this.oe = this.le / this.a;
 
     ellipse(ctx, 0, 0, this.a, this.b);
     stroke(ctx, colors.ink);
